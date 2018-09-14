a2be9e397d95c600dc43002fc705e714cc089658
saishav <saishav7@gmail.com>
2018-09-15 01:13:46 +1000

    appengine config

--- src/main/kotlin/io/saishav/crypto/CryptoApplication.kt
+++ src/main/kotlin/io/saishav/crypto/CryptoApplication.kt
@@ -2,9 +2,10 @@ package io.saishav.crypto
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
-class CryptoApplication
+class CryptoApplication: SpringBootServletInitializer()
 
 fun main(args: Array<String>) {
     runApplication<CryptoApplication>(*args)
